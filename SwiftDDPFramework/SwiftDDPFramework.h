d6460be8bc692ff3e7efe58d54e6f36533db124b
Shawn Bierman <shawnbierman@icloud.com>
2019-10-29 14:30:26 -0500

    Initial Commit

---
+++ SwiftDDPFramework/SwiftDDPFramework.h
@@ -0,0 +1,19 @@
+//
+//  SwiftDDPFramework.h
+//  SwiftDDPFramework
+//
+//  Created by Shawn Bierman on 10/29/19.
+//  Copyright © 2019 Methodist Le Bonheur Healthcare. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftDDPFramework.
+FOUNDATION_EXPORT double SwiftDDPFrameworkVersionNumber;
+
+//! Project version string for SwiftDDPFramework.
+FOUNDATION_EXPORT const unsigned char SwiftDDPFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDDPFramework/PublicHeader.h>
+
+
